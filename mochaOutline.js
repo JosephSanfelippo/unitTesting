c7b8d3e9684570beb128ea0377d48b7a39178339
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-06-01 20:16:18 -0500

    mochaOutline cleanup

--- mochaOutline.js
+++ mochaOutline.js
@@ -1,22 +1,24 @@
+//==========|Non-Functioning Outline|==========//
 describe('Math', () => {
   describe('.min', () => {
     it('returns the argument with the lowest value', () => {
-
+      //tests go here
     })
     it('returns -Infinity when no arguments are provided', () => {
-
+      //tests go here
     })
   })
 })
 
-//============================================//
-
-// import assert here
+//==========|Working Example|==========//
 const assert = require('assert');
 
 describe('+', () => {
   it('returns the sum of its arguments', () => {
-    // Write assertion here
     assert.ok(3 + 4 === 7);
   });
 });
+
+//==========|Setup||Exercise||Verify|==========//
+
+
