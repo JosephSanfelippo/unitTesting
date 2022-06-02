6799bba72866544592c5f45ceeaf925b4213eaec
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-06-01 19:50:55 -0500

    mochaOutline

---
+++ mochaOutline.js
@@ -0,0 +1,10 @@
+describe('Math', () => {
+  describe('.min', () => {
+    it('returns the argument with the lowest value', () => {
+
+    })
+    it('returns -Infinity when no arguments are provided', () => {
+
+    })
+  })
+})
