795e6535d410b69dc641b9685e702c5eec8287cb
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-06-01 20:09:16 -0500

    mochaOutline examples added

--- mochaOutline.js
+++ mochaOutline.js
@@ -8,3 +8,15 @@ describe('Math', () => {
     })
   })
 })
+
+//============================================//
+
+// import assert here
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of its arguments', () => {
+    // Write assertion here
+    assert.ok(3 + 4 === 7);
+  });
+});
